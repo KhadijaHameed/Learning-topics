3580ee219f0146a8d000a9e7327732e035539698
Muhammad Shams <uhfsolution@gmail.com>
2020-08-24 10:40:23 +0500

    init stack pkg

---
+++ app/src/main/java/com/example/stack/StackActivity.kt
@@ -0,0 +1,22 @@
+package com.example.stack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.roomwrapperimplement.R
+
+class StackActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stack)
+        initViews()
+
+    }
+
+    private fun initViews() {
+      //  etNewWord = findViewById(R.id.et_word)
+
+    }
+
+}
